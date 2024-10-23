afc027ea76c8a505bdafeefa29edf1e2a7b2f56b
Martin Etxeberria <Martin Etxeberria@DESKTOP-M0HE2JN>
2024-10-23 13:13:20 +0200

    Singleton

--- src/domain/Covid19Pacient.java
+++ src/domain/Covid19Pacient.java
@@ -7,14 +7,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Set;
+
+import factory.SymptomFactory;
 import iterator.Covid19PacientIterator;
 
 public class Covid19Pacient extends Observable{
 	private String  name; 
 	private int age;
 	private Map<Symptom,Integer> symptoms=new HashMap<Symptom,Integer>();
-	
-	private SymptomFactory sf=SymptomFactory.getInstance();
+
 	
 
 	public Covid19Pacient(String name, int years) {
@@ -59,7 +60,7 @@ public class Covid19Pacient extends Observable{
 	public Symptom addSymptomByName(String symptom, Integer w){
 		Symptom s=getSymptomByName(symptom);
 		if (s==null) {
-			s=sf.createSymptom(symptom); 
+			s=SymptomFactory.createSymptom(symptom); 
 			symptoms.put(s,w);		
 		}
 		return s;
