9f8bdda5a1394864619e3101f0aa7b9afb5ec46b
Martin Etxeberria <Martin Etxeberria@DESKTOP-M0HE2JN>
2024-11-04 11:52:31 +0100

    observer

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -10,9 +10,15 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-			Covid19Pacient pacient = new Covid19Pacient("Aitor", 35);
-			new PacientObserverGUI(pacient);
-			new PacientSymptomGUI(pacient);
+			Covid19Pacient pacient1 = new Covid19Pacient("Aitor", 35);
+			new PacientObserverGUI(pacient1);
+			new PacientSymptomGUI(pacient1);
+			
+			
+			Covid19Pacient pacient2 = new Covid19Pacient("Mikel", 55);
+			new PacientObserverGUI(pacient2);
+			new PacientSymptomGUI(pacient2);
+			new PacientThermometerGUI();
 	}
 
 
