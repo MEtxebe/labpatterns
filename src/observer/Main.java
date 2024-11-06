d634c00da654358b8bd70a80e4beb767f59f02a9
Martin Etxeberria <Martin Etxeberria@192.168.13.238>
2024-11-06 17:24:33 +0100

    Se me había olvidao xd

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -3,6 +3,7 @@ package observer;
 import java.util.Observable;
 
 import domain.Covid19Pacient;
+import factory.SymptomFactory;
 
 public class Main {
 
@@ -10,12 +11,14 @@ public class Main {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-			Covid19Pacient pacient1 = new Covid19Pacient("Aitor", 35);
+		SymptomFactory sf = new SymptomFactory();
+		
+			Covid19Pacient pacient1 = new Covid19Pacient("Aitor", 35, sf);
 			new PacientObserverGUI(pacient1);
 			new PacientSymptomGUI(pacient1);
 			
 			
-			Covid19Pacient pacient2 = new Covid19Pacient("Mikel", 55);
+			Covid19Pacient pacient2 = new Covid19Pacient("Mikel", 55, sf);
 			new PacientObserverGUI(pacient2);
 			new PacientSymptomGUI(pacient2);
 			new PacientThermometerGUI(pacient2);
