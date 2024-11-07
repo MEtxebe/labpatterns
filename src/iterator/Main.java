505ffd05683ef20023fd7d6611de517e2c148627
Martin Etxeberria <Martin Etxeberria@DESKTOP-M0HE2JN>
2024-11-07 12:31:15 +0100

    Aparcao

--- src/iterator/Main.java
+++ src/iterator/Main.java
@@ -18,11 +18,11 @@ import factory.SymptomFactory;
 		public static void main(String[] args) {
 			SymptomFactory sf = new SymptomFactory();
 			Covid19Pacient p=new Covid19Pacient("Ane", 29, sf);
-			p.addSymptom(new Symptom("s1", 10, 10), 1);
-			p.addSymptom(new Symptom("s2", 10, 10), 2);
-			p.addSymptom(new Symptom("s5", 10, 10), 1);
-			p.addSymptom(new Symptom("s3", 10, 10), 3);
-			p.addSymptom(new Symptom("s9", 10, 10), 2);
+			p.addSymptom(new Symptom("s1", 10, 1), 1);
+			p.addSymptom(new Symptom("s2", 10, 20), 2);
+			p.addSymptom(new Symptom("s5", 10, 5), 5);
+			p.addSymptom(new Symptom("s3", 10, 30), 3);
+			p.addSymptom(new Symptom("s9", 10, 9), 9);
 			
 			Covid19PacientAdapter i = new Covid19PacientAdapter(p);
 			
@@ -34,22 +34,20 @@ import factory.SymptomFactory;
 				System.out.println(i.previous());
 			}
 			
-			Sorting.sortedIterator(i, comName);		
+			
+			Iterator it= Sorting.sortedIterator(i, comName);
 			System.out.println("Izenen arabera ordenatua");
-			while(i.hasPrevious()) {
-				System.out.println(i.previous());
+			while(it.hasNext()) {
+				System.out.println(it.next());
 			}	
 			
-			Sorting.sortedIterator(i, comInd);		
+			it=Sorting.sortedIterator(i, comInd);	
 			System.out.println("Larritasunaren arabera ordenatua");
-			while(i.hasPrevious()) {
-				System.out.println(i.previous());
+			while(it.hasNext()) {
+				System.out.println(it.next());
 			}	
 			
-			
-			
-			
-			
+				
 		}
 	}
 
